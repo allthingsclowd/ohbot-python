ee582fa552e4508fe62cbd2caa672faa39e68413
Graham Land <graham@allthingscloud.eu>
2018-10-30 22:55:48 +0000

    added service setup

---
+++ examples/create_halloween_service.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+create_service () {
+  if [ ! -f /etc/systemd/system/${1}.service ]; then
+    
+    create_service_user ${1}
+    
+    sudo tee /etc/systemd/system/${1}.service <<EOF
+### BEGIN INIT INFO
+# Provides:          ${1}
+# Required-Start:    $local_fs $remote_fs
+# Required-Stop:     $local_fs $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: ${1} agent
+# Description:       ${2}
+### END INIT INFO
+[Unit]
+Description=${2}
+Requires=network-online.target
+After=network-online.target
+[Service]
+User=${1}
+Group=${1}
+PIDFile=/var/run/${1}/${1}.pid
+PermissionsStartOnly=true
+ExecStartPre=-/bin/mkdir -p /var/run/${1}
+ExecStartPre=/bin/chown -R ${1}:${1} /var/run/${1}
+ExecStart=${3}
+ExecReload=/bin/kill -HUP ${MAINPID}
+KillMode=process
+KillSignal=SIGTERM
+Restart=on-failure
+RestartSec=42s
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl daemon-reload
+
+  fi
+
+}
+
+create_service_user () {
+  
+  if ! grep ${1} /etc/passwd >/dev/null 2>&1; then
+    echo "Creating ${1} user to run the consul service"
+    sudo useradd --system --home /etc/${1}.d --shell /bin/false ${1}
+    sudo mkdir --parents /opt/${1} /usr/local/${1} /etc/${1}.d
+    sudo chown --recursive ${1}:${1} /opt/${1} /etc/${1}.d /usr/local/${1}
+  fi
+
+}
+
+create_service halloween "halloween robot service" "docker container run -dit --name ohbot10 --device /dev/ttyACM0 --device /dev/snd --device /dev/gpiomem allthingscloud/ohbot-rpi"
